c49ead0d65aa19ccc066a8a65307f9bae3b9ce22
Lucaci Diana <diana.lucaci22@yahoo.com>
2019-12-01 22:59:10 -0500

    remove words from vocab

--- rationale_net/corpus_analysis/vocabulary.py
+++ rationale_net/corpus_analysis/vocabulary.py
@@ -29,7 +29,8 @@ class TfidfVocabulary(Vocabulary):
     """
     def __init__(self, args, data, vocab_dim=400):
         Vocabulary.__init__(self, data)
-        self.path = args.vocab_path + "tfidf-200.txt"
+        self.args = args
+        self.path = args.vocab_path + "tfidf-400.txt"
         self.max_dim=vocab_dim
         # Number of documents = number of distinc labels
         self.N = len(self.class_documents.keys())
@@ -39,7 +40,12 @@ class TfidfVocabulary(Vocabulary):
         # {doc_name:{word:score}}
         self.tf_idf = self.tf_idf_scores()
         print(f"Corpus of {sum([len(words) for doc, words in self.class_documents.items()])}")
-
+        if self.args.aspect =="aroma":
+            self.ignored_words = ["snowboarding"]
+        elif self.args.aspect =="palate":
+            self.ignored_words = ["barbershop"]
+        elif self.args.aspect == "appearance":
+            self.ignored_words = ["lodi", "shirley", "duff"]
     def t_freq(self):
         tf = {}
         term_freq = {}
@@ -66,17 +72,39 @@ class TfidfVocabulary(Vocabulary):
             tf_idf[doc_name] = tf_idf_doc
         return tf_idf
 
+    def ignore_words(self, word_list):
+        """
+        Returns: 
+            list of removed words (that are in self.ignored_words)
+            list with the remaining words
+        """
+        remove = []
+        for word in word_list:
+            if word.lower().strip() in self.ignored_words:
+                remove.append(word)
+                word_list.remove(word)
+        return remove, word_list
+                
+
     def possible_explanations(self):
         """
         Return a list of possible explanations (as text/strings)
         equally distributed among the documents
         """
-        explanations = []
-        for doc in self.class_documents.keys():
-            ranked_words = sorted(self.tf_idf[doc], key=self.tf_idf[doc].get, reverse=True)
-            explanations.extend(ranked_words[:self.max_dim//self.N])
-        expl = {i: explanation for i,explanation in enumerate(explanations)}
+        doc_count = {}
+        with open(self.args.vocab_path+"doc-distr.txt", "w") as f:
+            explanations = []
+            for doc in self.class_documents.keys():
+                ranked_words = sorted(self.tf_idf[doc], key=self.tf_idf[doc].get, reverse=True)
+                removed_words, remaining_words = self.ignore_words(ranked_words)
+                explanations.extend(remaining_words[:self.max_dim//self.N])
+                print(f"doc: {doc}\n{ranked_words}\nRemoved:\n{removed_words}~~~", file=f)
+                doc_count[doc] = len(remaining_words)
+            expl = {i: explanation for i,explanation in enumerate(explanations)}
         with open(self.path, 'w') as f:
             f.write(str(expl))
+            f.write("Dict len:")
+            f.write(str(len(expl)))
+            f.write(f"Doc distribution\n{doc_count}")
         return expl
 
