40b15088ac93b0256252d4ee5dd322a258e6dbe4
Lucaci Diana <diana.lucaci22@yahoo.com>
2020-01-20 10:13:46 -0500

    old changes

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ import scripts.args as generic
 import rationale_net.utils.model_train as model_helper
 import os
 import pickle
-
-
+import numpy as np
+import torch
 
 if __name__ == '__main__':
+    
+    torch.manual_seed(0)
+    if torch.cuda.is_available(): torch.cuda.manual_seed_all(0)
     # update args and print
     args = generic.parse_args()
     print("Loading embeddings")
@@ -21,17 +24,24 @@ if __name__ == '__main__':
     
  
     train_data, dev_data, test_data, explanation_vocab = dataset_factory.get_dataset(args, word_to_indx)
-
+    args.expl_vocab = torch.tensor([explanation_vocab[e_id]["emb"].data for e_id in sorted(explanation_vocab.keys())])
+    config = {}
+    config["expl_text"] = np.array([explanation_vocab[e_id]["text"] for e_id in sorted(explanation_vocab.keys())])
+    
+    if args.cuda:
+        args.expl_vocab = args.expl_vocab.cuda()
     results_path_stem = args.results_path.split('/')[-1].split('.')[0]
     args.model_path = '{}.pt'.format(os.path.join(args.save_dir, results_path_stem))
     print(args.model_path)
     # model
     gen, model = model_factory.get_model(args, embeddings, train_data, explanation_vocab)
 
+
+
     print()
     # train
     if args.train :
-        epoch_stats, model, gen = model_helper.train_model(train_data, dev_data, model, gen, args)
+        epoch_stats, model, gen = model_helper.train_model(train_data, dev_data, model, gen, args, config)
         args.epoch_stats = epoch_stats
         save_path = args.results_path
         print("Save train/dev results to", save_path)
@@ -41,7 +51,7 @@ if __name__ == '__main__':
 
     # test
     if args.test :
-        test_stats = model_helper.test_model(test_data, model, gen, args)
+        test_stats = model_helper.test_model(test_data, model, gen, args, config)
         args.test_stats = test_stats
         args.train_data = train_data
         args.test_data = test_data
