0e9d3d4529677f2ad186e870c95f0588a3511d20
Lucaci Diana <diana.lucaci22@yahoo.com>
2020-01-30 17:26:14 -0500

    regex for <br> tag removal fix

--- corpus_explanation/datasets/preprocessing.py
+++ corpus_explanation/datasets/preprocessing.py
@@ -2,6 +2,6 @@ from torchtext.data import Pipeline
 import re
 
 def remove_br_tag(token):
-    return re.sub(r"<(.*?)>", "", token)
+    return re.sub(r"br|(/><.*)|(</?(.*)/?>)|(<?(.*)/?>)|(<?/?(.*?)/?>?)", "", token)
 
 remove_br_html_tags = Pipeline(remove_br_tag)
