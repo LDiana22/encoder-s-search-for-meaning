9814c73e1f757a2e7669393dc5412c7c0038ce25
Diana Lucaci <dluca058@gra-login3.graham.sharcnet>
2020-02-07 19:48:54 -0500

    run try

--- corpus_explanation/run.sh
+++ corpus_explanation/run.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-#SBATCH --time=05:45:00
+#SBATCH --time=25:45:00
 #SBATCH --mail-user=dluca058@uottawa.ca
 #SBATCH --mail-type=END
 #SBATCH --mail-type=BEGIN
 #SBATCH --mem=32G
-#SBATCH --nodes=1
 #SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=3
+#SBATCH --cpus-per-task=2
 #SBATCH --output=out/%x-%j.out
-#SBATCH --job-name=vanila
+#SBATCH --job-name=nodes-read-all
 
 module load python/3.6
 
