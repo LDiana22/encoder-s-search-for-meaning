b0973050619c39f6c8f9bb38f35adfa17d456838
LDiana22 <diana.lucaci22@yahoo.com>
2020-03-26 18:09:00 -0400

    indentation

--- corpus_explanation/notebooks/notebook_multi_word_mlp.py
+++ corpus_explanation/notebooks/notebook_multi_word_mlp.py
@@ -59,34 +59,33 @@ import re
 from datetime import datetime
 
 def _extract_date(f):
-    date_string = re.search(f"{DATE_REGEXP}",f)[0]
+    date_string = re.search(f"^{DATE_REGEXP}",f)[0]
     return datetime.strptime(date_string, DATE_FORMAT)
 
 def get_max_index_checkpoint(path):
-  """
-  Return int: suffix of checkpoint name
-  """
-  list_of_files = os.listdir(path)
-  # list_of_files = ["checkpoint_1","checkpoint_10","checkpoint_2", "checkpoint_22"]
+    """
+    Return int: suffix of checkpoint name
+    """
+    list_of_files = os.listdir(path)
+    # list_of_files = ["checkpoint_1","checkpoint_10","checkpoint_2", "checkpoint_22"]
 
-  n = max([_extract_number(f) for f in list_of_files]) if list_of_files else None
-  if n is None:
-    return 0
-
-  return n
+    n = max([_extract_number(f) for f in list_of_files]) if list_of_files else None
+    if n is None:
+        return 0
+    return n
 
 def get_last_checkpoint_by_date(path):
-  """
-  Return file_name with the largest suffix number
-  """
-  list_of_files = os.listdir(path)
+    """
+    Return file_name with the largest suffix number
+    """
+    list_of_files = os.listdir(path)
 
-  file_dates = {_extract_date(f): f for f in list_of_files}
-  if file_dates:
-    key = sorted(file_dates.keys(), reverse=True)[0]
-    return file_dates[key]
-  else:
-    return None
+    file_dates = {_extract_date(f): f for f in list_of_files}
+    if file_dates:
+        key = sorted(file_dates.keys(), reverse=True)[0]
+        return file_dates[key]
+    else:
+        return None
 
 # %% [markdown]
 # ## Experiment
