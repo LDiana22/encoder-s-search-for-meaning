softmax cuda fix max vocab

corpus_explanation/notebooks/softmax-emb.py
@@ -4,13 +4,55 @@
 
 # %% [code]
 # -*- coding: utf-8 -*-
+from abc import ABC
+from collections import OrderedDict 
+from collections import ChainMap
+from contextlib import redirect_stdout
+import glob
+import io
+import itertools
+import os
+import os.path
+import pickle
+import random
+import re
+import spacy
+import string
+import time
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+from datetime import datetime
+
 import torch
-torch.manual_seed(0)
-torch.cuda.manual_seed(0)
-torch.backends.cudnn.deterministic=True
-torch.backends.cudnn.benchmark=False
+
+from torch import nn
+from torch.utils import data as utils
+
+import torch.nn.functional as F
+
+import torch.optim as optim
+
+from torchtext import datasets
+from torchtext import data as data
+from torchtext.vocab import GloVe
+from torchtext.data import Pipeline
+
 import numpy as np
+
+import yake
+from summa import keywords
+from rake_nltk import Rake
+
+
+torch.manual_seed(0)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark=False
+
+torch.cuda.manual_seed(0)
 np.random.seed(0)
+
 
 VECTOR_CACHE = "../.vector_cache"
 UCI_PATH = "../.data/uci"
@@ -55,7 +97,7 @@ CONFIG = {
         },
 
     "aspect": "palate", # aroma, palate, smell, all
-    "max_vocab_size": 25000,
+    "max_vocab_size": 400000,
     "emb_dim": 300,
     "batch_size": 32,
     "output_dim": 1,
@@ -68,19 +110,11 @@ DATE_REGEXP = '[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2}'
 
 """# Helpers"""
 
-import os
-import re
 
-from datetime import datetime
 
 def _extract_date(f):
     date_string = re.search(f"^{DATE_REGEXP}",f)[0]
     return datetime.strptime(date_string, DATE_FORMAT)
-
-import os
-import re
-
-from datetime import datetime
 
 def _extract_date(f):
     date_string = re.search(f"^{DATE_REGEXP}",f)[0]
@@ -113,10 +147,6 @@ def get_last_checkpoint_by_date(path):
   else:
     return None
 
-from torchtext.data import Pipeline
-import re
-
-import string
 
 def remove_br_tag(token):
     return re.sub(r"br|(/><.*)|(</?(.*)/?>)|(<?(.*)/?>)|(<?/?(.*?)/?>?)", "", token)
@@ -135,10 +165,7 @@ preprocess_pipeline = Pipeline(preprocess)
 
 """# Experiment"""
 
-import torch
-import time
-from datetime import datetime
-import os
+
 
 class Experiment(object):
     """Holds all the experiment parameters and provides helper functions."""
@@ -274,19 +301,6 @@ class Experiment(object):
 ### IMDB
 """
 
-import glob
-import os
-import io
-import random
-import re
-import spacy
-from torchtext import datasets
-from torchtext import data as data
-from torchtext.vocab import GloVe
-import torch
-
-torch.manual_seed(0)
-torch.backends.cudnn.deterministic = True
 
 class IMDBDataset:
 
@@ -376,8 +390,7 @@ class IMDBDataset:
 
 """### UCI"""
 
-from torch.utils import data as utils
-import numpy as np
+
 class UCIDataset:
   
   def __init__(self, args, max_length=250):
@@ -478,9 +491,7 @@ class UCIDataset:
 ## Abstract
 """
 
-import os
-import pickle
-import re
+
 
 class AbstractDictionary:
   def __init__(self, id, dataset, args):
@@ -577,13 +588,6 @@ class AbstractDictionary:
 
 # !pip install rake_nltk
 
-import pickle
-import os
-from rake_nltk import Rake
-from collections import OrderedDict 
-
-import spacy
-from collections import ChainMap
 
 class RakePerClassExplanations(AbstractDictionary):
 
@@ -737,8 +741,7 @@ a
 
 # pip install summa
 
-from summa import keywords
-import itertools
+
 class TextRank(AbstractDictionary):
 
   def __init__(self, id, dataset, args): 
@@ -774,8 +777,6 @@ class TextRank(AbstractDictionary):
 
 """## TF-IDF"""
 
-import itertools
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 class TFIDF(AbstractDictionary):
 
   def __init__(self, id, dataset, args): 
@@ -822,8 +823,7 @@ class TFIDF(AbstractDictionary):
 
 # pip install git+https://github.com/LIAAD/yake
 
-import yake
-import itertools
+
 class DefaultYAKE(AbstractDictionary):
   def __init__(self, id, dataset, args): 
     super().__init__(id, dataset, args)
@@ -861,16 +861,7 @@ class DefaultYAKE(AbstractDictionary):
 ## Abstract
 """
 
-from abc import ABC
-import os.path
-from datetime import datetime
-import torch
-from contextlib import redirect_stdout
-from torch import nn
-from datetime import datetime
 
-
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 class AbstractModel(nn.Module):
     """
@@ -1079,10 +1070,6 @@ class AbstractModel(nn.Module):
 
 """## Vanilla"""
 
-import torch
-from torch import nn
-import torch.optim as optim
-
 class VLSTM(AbstractModel):
     """
     Baseline - no generator model
@@ -1153,14 +1140,6 @@ class VLSTM(AbstractModel):
 
 """## MLP"""
 
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-import torch
-from torch import nn
-import torch.optim as optim
-from collections import OrderedDict 
-import numpy as np
-
-import torch.nn.functional as F
 
 class MLPGen(AbstractModel):
     """
@@ -1512,7 +1491,6 @@ class MLPGen(AbstractModel):
 
 """# Main"""
 
-from datetime import datetime
 
 start = datetime.now()
 formated_date = start.strftime(DATE_FORMAT)
@@ -1749,7 +1727,6 @@ print(f"Time: {str(datetime.now()-start)}")
 
 # print(f"Time: {str(datetime.now()-start)}")
 
-from datetime import datetime
 
 start = datetime.now()
 formated_date = start.strftime(DATE_FORMAT)
@@ -1778,7 +1755,6 @@ experiment.with_data(dataset).with_dictionary(explanations).with_model(model).ru
 
 print(f"Time: {str(datetime.now()-start)}")
 
-from datetime import datetime
 
 start = datetime.now()
 formated_date = start.strftime(DATE_FORMAT)
@@ -1807,7 +1783,6 @@ experiment.with_data(dataset).with_dictionary(explanations).with_model(model).ru
 
 print(f"Time: {str(datetime.now()-start)}")
 
-from datetime import datetime
 
 start = datetime.now()
 formated_date = start.strftime(DATE_FORMAT)