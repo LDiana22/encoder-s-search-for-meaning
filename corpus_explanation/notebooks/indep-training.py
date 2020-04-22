0c85c4798ca8a43c3230252262501b14173cd49d
LDiana22 <diana.lucaci22@yahoo.com>
2020-04-22 17:44:18 -0400

    ibefore fix

--- corpus_explanation/notebooks/indep-training.py
+++ corpus_explanation/notebooks/indep-training.py
@@ -1984,11 +1984,14 @@ class MLPIndependentOneDict(AbstractModel):
 ##########################################################################################################
 ############################################ MLP before frozen bi-LSTM ##################################
 ##########################################################################################################
-
 class MLPBefore(MLPIndependentOneDict):
   """
   MLP + pretrained bi-LSTM
   """
+  def __init__(self, id, mapping_file_location, model_args, dataset, explanations):
+    super().__init__(id, mapping_file_location, model_args, dataset, explanations)
+    self.lin_pos = nn.Linear(model_args["hidden_dim"], len(self.dictionary.keys())).to(self.device)
+
   
   def forward(self, text, text_lengths, expl_file=None):
 
@@ -2027,6 +2030,62 @@ class MLPBefore(MLPIndependentOneDict):
     self.expl_distributions = expl_distributions
 
     return output
+
+  def gen(self, activ, batch_size):
+    context_vector, final_dict, expl_distributions = [], [], []
+    # [dict_size, max_words, emb_dim]
+    # explanations[i] -> [dict_size, max_words, emb_dim]
+    v_emb_pos = self.embedding(self.explanations)
+    #[batch,dict_size, max_words, emd_dim
+    vocab_emb_pos = v_emb_pos.repeat(batch_size,1,1,1)
+
+    #[batch,dict_size, max_words* emd_dim]
+    vocab_emb_pos = vocab_emb_pos.reshape(vocab_emb_pos.size(0),vocab_emb_pos.size(1),-1)
+
+    # [sent, batch, dict_size]
+    expl_activ_pos = self.lin_pos(activ)
+
+    # [batch, sent, dict_size]
+    expl_distribution_pos = torch.transpose(expl_activ_pos, 0, 1)
+    
+    # [batch, max_sent, dict_size] (pad right)
+    size1, size2, size3 = expl_distribution_pos.shape[0], expl_distribution_pos.shape[1], expl_distribution_pos.shape[2]
+    if self.max_sent_len>=size2:
+        # 0-padding
+        expl_distribution_pos = torch.cat([expl_distribution_pos, expl_distribution_pos.new(size1, self.max_sent_len-size2, size3).zero_()],1).to(self.device)
+    else:
+        # trimming
+        expl_distribution_pos = expl_distribution_pos[:,:self.max_sent_len,:]
+    
+
+
+    # [batch,dict_size, sent]
+    e_pos = torch.transpose(expl_distribution_pos,1,2)
+    # [batch, dict, 1]
+    expl_distribution_pos = self.aggregation_pos(e_pos).squeeze()
+    # expl_distribution = self.sigmoid(expl_distribution) # on dim 1
+    expl_distribution_pos = F.gumbel_softmax(expl_distribution_pos, hard=True)
+
+    # expl_distribution_pos = self.softmax(expl_distribution_pos)
+    # expl_distribution_neg = self.softmax(expl_distribution_neg)
+    #[batch, dict]
+    # expl_distributions.append(torch.squeeze(expl_distribution_pos))
+
+    # [batch,1, dict]
+    e_dist_pos = torch.transpose(expl_distribution_pos.unsqueeze(-1),1,2)
+    # batch, 1, dict x batch, dict, emb (max_words*emb_dim)
+    expl_pos = torch.bmm(e_dist_pos, vocab_emb_pos)
+
+    # #[batch,max_words,emb_dim]
+    context_vector.append(torch.max(expl_pos, dim=1).values.reshape(batch_size, v_emb_pos.size(1),-1))
+
+
+    sep = torch.zeros((batch_size,1,self.emb_dim), device=self.device)
+    # [batch, 1+1, emb_dim]
+    # final_dict.append(torch.cat((sep, context_vector[0]), 1))
+
+    return torch.cat((sep, context_vector[0]), 1), torch.squeeze(expl_distribution_pos)
+
 
 
 
