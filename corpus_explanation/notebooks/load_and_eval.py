05d163dd7578b82109e08a2db6d5703a7b30acf7
LDiana22 <diana.lucaci22@yahoo.com>
2020-05-19 10:45:46 -0400

    abstract model

--- corpus_explanation/notebooks/load_and_eval.py
+++ corpus_explanation/notebooks/load_and_eval.py
@@ -209,6 +209,214 @@ class RakeCorpusPolarityFiltered(AbstractDictionary):
 
 
 
+class AbstractModel(nn.Module):
+    """
+    Abstract Model
+        - saves the mapping between the model-id and its parameters and
+            model summary
+        - creates the directories for the log files
+    """
+    def __init__(self, id, mapping_file_location, model_args):
+        """
+        id: Model id
+        mapping_file_location: directory to store the file "model_id" 
+                               that containes the hyperparameters values and 
+                               the model summary
+        logs_location: directory for the logs location of the model
+        model_args: hyperparameters of the model
+        """
+        super().__init__()
+        self.delim = "#################################"
+        self.id = id
+        self.mapping_location = mapping_file_location
+        self.args = model_args
+        self.epoch=-1
+        self.device = torch.device('cuda' if model_args["cuda"] else 'cpu')
+        self.model_dir = model_dir = os.path.join(self.args["prefix_dir"], self.id)
+        self.__create_directories()
+
+    def override(self, args):
+        self.args.update(args)
+
+    def __create_directories(self):
+        """
+        All the directories for a model are placed under the directory 
+            prefix_dir / model_id / {dirs}
+        """ 
+        self.checkpoint_dir = os.path.join(self.model_dir, self.args["dirs"]["checkpoint"])
+        for directory in self.args["dirs"].values():
+            m_dir = os.path.join(self.model_dir, directory)
+            if not os.path.isdir(m_dir):
+                os.makedirs(m_dir)
+        if not os.path.isdir(self.mapping_location):
+            os.makedirs(self.mapping_location)
+
+    def save_model_type(self, model):
+        """
+        Saves the hyperparameters 
+        """
+        mapping_file = os.path.join(self.mapping_location, self.id)        
+        with open(mapping_file, "w") as map_file:
+            print(self.delim, file=map_file)
+            print(self.args, file=map_file)
+            print(self.delim, file=map_file)
+            print(self, file=map_file)
+            print(self.delim, file=map_file)
+
+    def checkpoint(self, epoch, metrics):
+        checkpoint_file = os.path.join(self.checkpoint_dir, 
+            f'{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}_e{epoch}')
+        self.dict_checkpoint = {
+            'epoch': epoch,
+            'model_state_dict': self.state_dict(),
+            'optimizer_state_dict': self.optimizer.state_dict(),
+            }
+        self.dict_checkpoint.update(metrics)
+        torch.save(self.dict_checkpoint, checkpoint_file)
+
+    def load_best_model(self):
+        newest_checkpoint = get_last_checkpoint_by_date(os.path.join(self.model_dir, self.args["dirs"]["checkpoint"]))
+        checkpoint_dir = os.path.join(self.model_dir, self.args["dirs"]["checkpoint"])           
+        newest_checkpoint = os.path.join(checkpoint_dir, newest_checkpoint)
+        print(f"Loading best model from {newest_checkpoint}")
+        self.load_checkpoint(newest_checkpoint)
+
+    def load_checkpoint(self, path):
+        print(f"Loading checkpoint: {path}") 
+        checkpoint = torch.load(path)
+        self.load_state_dict(checkpoint['model_state_dict'])
+        self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        self.epoch = checkpoint['epoch']
+        self.metrics = {}
+        for key in checkpoint.keys():
+            if key not in ['epoch', 'model_state_dict', 'optimizer_state_dict']:
+                self.metrics[key] = checkpoint[key]
+
+    def get_plot_path(self, file_suffix ):
+        dir_path = os.path.join(self.model_dir, self.args["dirs"]["plots"])
+        return os.path.join(dir_path, f"{file_suffix}_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.png")
+
+    def save_results(self, metrics, file_suffix=""):
+        metrics_path = os.path.join(self.model_dir, self.args["dirs"]["metrics"])
+        results_file = os.path.join(metrics_path, f"results_{file_suffix}_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.txt")
+        with open(results_file, "w") as f:
+            f.write(str(metrics))
+            f.write("\n\n")
+            f.write("Loss, acc, prec, rec, F1, macroF1, microF1, weightedF1\n")
+            metric_names = ['test_f_loss', 'test_f_acc', 'test_f_prec', 'test_f_rec', 'test_f_f1', 'test_f_macrof1', 'test_f_microf1', 'test_f_weightedf1']
+            res = []
+            for metric in metric_names:
+                res += ["{0:.4f}".format(metrics.get(metric, -1))]
+            f.write(" & ".join(res))
+
+    def train_model(self, iterator, args=None):
+        """
+        metrics.keys(): [train_acc, train_loss, train_prec,
+                        train_rec, train_f1, train_macrof1,
+                        train_microf1, train_weightedf1]
+        e.g. metrics={"train_acc": 90.0, "train_loss": 0.002}
+        """
+        e_loss = 0
+        e_acc, e_prec, e_rec = 0,0,0
+        e_f1, e_macrof1, e_microf1, e_wf1 = 0,0,0,0
+
+        self.train()
+
+        for batch in iterator:
+            self.optimizer.zero_grad()
+            text, text_lengths = batch.text
+            logits = self.forward(text, text_lengths).squeeze()
+            batch.label = batch.label.to(self.device)
+            loss = self.criterion(logits, batch.label)
+
+            y_pred = torch.round(torch.sigmoid(logits)).detach().cpu().numpy()
+            y_true = batch.label.cpu().numpy()
+            #metrics
+            acc = accuracy_score(y_true, y_pred)
+            prec = precision_score(y_true, y_pred)
+            rec = recall_score(y_true, y_pred)
+            f1 = f1_score(y_true, y_pred)
+            macrof1 = f1_score(y_true, y_pred, average='macro')
+            microf1 = f1_score(y_true, y_pred, average='micro')
+            wf1 = f1_score(y_true, y_pred, average='weighted')
+
+            loss.backward()
+            self.optimizer.step()
+
+            e_loss += loss.item()
+            e_acc += acc
+            e_prec += prec
+            e_rec += rec
+            e_f1 += f1
+            e_macrof1 += macrof1
+            e_microf1 += microf1
+            e_wf1 += wf1
+
+        metrics ={}
+        size = len(iterator)
+        metrics["train_loss"] = e_loss/size
+        metrics["train_acc"] = e_acc/size
+        metrics["train_prec"] = e_prec/size
+        metrics["train_rec"] = e_rec/size
+        metrics["train_f1"] = e_f1/size
+        metrics["train_macrof1"] = e_macrof1/size
+        metrics["train_microf1"] = e_microf1/size
+        metrics["train_weightedf1"] = e_wf1/size
+
+        return metrics
+
+    def evaluate(self, iterator, prefix="test"):
+        """
+            Return a metrics dict with the keys prefixed by prefix
+            metrics = {}
+            e.g. metrics={f"{prefix}_acc": 90.0, f"{prefix}_loss": 0.002}
+        """
+        self.eval()
+
+        e_loss = 0
+        e_acc, e_prec, e_rec = 0,0,0
+        e_f1, e_macrof1, e_microf1, e_wf1 = 0,0,0,0
+        with torch.no_grad():
+            for batch in iterator:
+                text, text_lengths = batch.text
+                logits = self.forward(text, text_lengths, prefix).squeeze()
+                batch.label = batch.label.to(self.device)
+                loss = self.criterion(logits, batch.label)
+
+                predictions = torch.round(torch.sigmoid(logits))
+
+                y_pred = predictions.detach().cpu().numpy()
+                y_true = batch.label.cpu().numpy()
+
+                acc = accuracy_score(y_true, y_pred)
+                prec = precision_score(y_true, y_pred)
+                rec = recall_score(y_true, y_pred)
+                f1 = f1_score(y_true, y_pred)
+                macrof1 = f1_score(y_true, y_pred, average='macro')
+                microf1 = f1_score(y_true, y_pred, average='micro')
+                wf1 = f1_score(y_true, y_pred, average='weighted')
+
+                e_loss += loss.item()
+                e_acc += acc
+                e_prec += prec
+                e_rec += rec
+                e_f1 += f1
+                e_macrof1 += macrof1
+                e_microf1 += microf1
+                e_wf1 += wf1
+
+        metrics ={}
+        size = len(iterator)
+        metrics[f"{prefix}_loss"] = e_loss/size
+        metrics[f"{prefix}_acc"] = e_acc/size
+        metrics[f"{prefix}_prec"] = e_prec/size
+        metrics[f"{prefix}_rec"] = e_rec/size
+        metrics[f"{prefix}_f1"] = e_f1/size
+        metrics[f"{prefix}_macrof1"] = e_macrof1/size
+        metrics[f"{prefix}_microf1"] = e_microf1/size
+        metrics[f"{prefix}_weightedf1"] = e_wf1/size
+        return metrics
+
 
 from math import log
 
