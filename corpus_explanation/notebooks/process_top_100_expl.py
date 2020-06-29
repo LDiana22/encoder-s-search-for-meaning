8120bbdb8c974f6a462e68041b60505291a484df
LDiana22 <diana.lucaci22@yahoo.com>
2020-06-29 13:33:34 -0400

    compute coherence for top c/i

--- corpus_explanation/notebooks/process_top_100_expl.py
+++ corpus_explanation/notebooks/process_top_100_expl.py
@@ -33,21 +33,13 @@ parser.add_argument('-f', metavar='file name', type=str,
                     help='expl file')
 parser.add_argument('--fix', metavar='fix the explanation file format', type=bool, default=False)
 
+parser.add_argument('-p', metavar='path', type=str,
+                    help='expl file')
+parser.add_argument('-f1', metavar='file name', type=str,
+                    help='expl file')
+parser.add_argument('-f2', metavar='file name', type=str,
+                    help='expl file')
 args = parser.parse_args()
-
-def merge_and_shuffle():
-	#path = "experiments\\independent\\bilstm_mlp_improve-dnn15-1-25-decay0.0-L2-dr0.3-eval1-rake-polarity-improveloss_mean-alpha0.7-c-e40-2020-05-26_01-06-16\\explanations\\descending_contribution_correct.txt"
-	path = "experiments/soa-dicts/rc_bilstm_mlp_improve_30-30_l20.01_dr0.8_lr0.01_soa_vlstm2-64-0.3_pretrained_rake-polarity-4-60-dnn30-1-30-decay0.0-L2-dr0.8-eval1-rake-polarity-4-600-improveloss_mean-alpha0.7-c-e10-2020-06-29_01-40-17/explanations/descending_contribution_correct.txt"
-        df1=load_explanations(path)
-	#path = "experiments\\independent\\bilstm_mlp_improve-dnn15-1-25-decay0.0-L2-dr0.3-eval1-rake-polarity-improveloss_mean-alpha0.7-c-e40-2020-05-26_01-06-16\\explanations\\descending_contribution_incorrect.txt"
-	path = "experiments/soa-dicts/rc_bilstm_mlp_improve_30-30_l20.01_dr0.8_lr0.01_soa_vlstm2-64-0.3_pretrained_rake-polarity-4-60-dnn30-1-30-decay0.0-L2-dr0.8-eval1-rake-polarity-4-600-improveloss_mean-alpha0.7-c-e10-2020-06-29_01-40-17/explanations/descending_contribution_incorrect.txt"
-        df2=load_explanations(path)
-	df = pd.concat([df1.head(50),df2.head(50)])
-	df = df.sample(frac=1).reset_index(drop=True)
-
-	df.to_csv("experiments\\independent\\bilstm_mlp_improve-dnn15-1-25-decay0.0-L2-dr0.3-eval1-rake-polarity-improveloss_mean-alpha0.7-c-e40-2020-05-26_01-06-16\\explanations\\100_posneg_influences_shuffled_contributions.csv")
-
-merge_and_shuffle()
 
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
@@ -60,11 +52,48 @@ def polarity(phrase):
 		return 0
 	return None
 
-path= "experiments\\independent\\bilstm_mlp_improve-dnn15-1-25-decay0.0-L2-dr0.3-eval1-rake-polarity-improveloss_mean-alpha0.7-c-e40-2020-05-26_01-06-16\\explanations\\100_shuffled_contributions.csv"
-print(f"Loading from {path}")
-df = pd.read_csv(path, sep=",",header=0, names=["id","review","explanation","contribution","frequency","confidence_score","prediction","label"])
-print(df.shape)
-df["Explanation_Sentiment"] = df["explanation"].apply(polarity)
-path = "experiments\\independent\\bilstm_mlp_improve-dnn15-1-25-decay0.0-L2-dr0.3-eval1-rake-polarity-improveloss_mean-alpha0.7-c-e40-2020-05-26_01-06-16\\explanations"
+def load_new_explanations(path):
+  print(f"Loading from {path}")
+  df = pd.read_csv(path, header=0, names=["id","review","explanation","contribution","frequency","confidence_score","prediction","label","raw_pred"], encoding="utf-8")
+  #df["contribution"] = df["contribution"].apply(lambda c: float(str(c).split(":")[0]))
+  df["contribution"] = df["contribution"].astype('float64')
+  df["frequency"] = df["explanation"].apply(lambda f: list(re.findall(r'(\d+)', str(f)))).apply(lambda x: x[0] if x else None)
+  df["confidence_score"] = df["confidence_score"].astype('float64')
+  df["prediction"] = df["prediction"].astype('float64')
+  df["label"] = df["label"].astype('float64')
+  df["raw_pred"] = df["raw_pred"].astype('float64')
+  df["explanation"] = df["explanation"].apply(lambda s: s.split("'")[1])
+  df["e_polarity"] = df["explanation"].apply(polarity)
+  return df
+
+
+def merge_and_shuffle(path1, path2):
+	#path = "experiments\\independent\\bilstm_mlp_improve-dnn15-1-25-decay0.0-L2-dr0.3-eval1-rake-polarity-improveloss_mean-alpha0.7-c-e40-2020-05-26_01-06-16\\explanations\\descending_contribution_correct.txt"
+	#path = "experiments\\soa-dicts\\rc_bilstm_mlp_improve_30-30_l20.01_dr0.8_lr0.01_soa_vlstm2-64-0.3_pretrained_rake-polarity-4-60-dnn30-1-30-decay0.0-L2-dr0.8-eval1-rake-polarity-4-600-improveloss_mean-alpha0.7-c-e10-2020-06-29_01-40-17\\explanations\\descending_contribution_correct.txt"
+  df1=load_new_explanations(path1)
+	#path = "experiments\\independent\\bilstm_mlp_improve-dnn15-1-25-decay0.0-L2-dr0.3-eval1-rake-polarity-improveloss_mean-alpha0.7-c-e40-2020-05-26_01-06-16\\explanations\\descending_contribution_incorrect.txt"
+	#path = "experiments\\soa-dicts\\rc_bilstm_mlp_improve_30-30_l20.01_dr0.8_lr0.01_soa_vlstm2-64-0.3_pretrained_rake-polarity-4-60-dnn30-1-30-decay0.0-L2-dr0.8-eval1-rake-polarity-4-600-improveloss_mean-alpha0.7-c-e10-2020-06-29_01-40-17\\explanations\\descending_contribution_incorrect.txt"
+  df2=load_new_explanations(path2)
+	df = pd.concat([df1.head(50),df2.head(50)])
+	df = df.sample(frac=1).reset_index(drop=True)
+
+	df.to_csv("experiments\\soa-dicts\\rc_bilstm_mlp_improve_30-30_l20.01_dr0.8_lr0.01_soa_vlstm2-64-0.3_pretrained_rake-polarity-4-60-dnn30-1-30-decay0.0-L2-dr0.8-eval1-rake-polarity-4-600-improveloss_mean-alpha0.7-c-e10-2020-06-29_01-40-17\\explanations\\100_posneg_shuffled_contributions.csv")
+
+	return df
+path1 = os.path.join(args.p, args.f1)
+path2 = os.path.join(args.p, args.f2)
+merged = merge_and_shuffle(path1, path2)
+print(merged.shape)
+print("Count polarity=label")
+print(merged[merged["e_polarity"]==merged["label"]].count()["id"])
+print("Count polarity=prediction")
+print(merged[merged["e_polarity"]==merged["prediction"]].count()["id"])
+
+# path= "experiments\\independent\\bilstm_mlp_improve-dnn15-1-25-decay0.0-L2-dr0.3-eval1-rake-polarity-improveloss_mean-alpha0.7-c-e40-2020-05-26_01-06-16\\explanations\\100_shuffled_contributions.csv"
+# print(f"Loading from {path}")
+# df = pd.read_csv(path, sep=",",header=0, names=["id","review","explanation","contribution","frequency","confidence_score","prediction","label"])
+# df["Explanation_Sentiment"] = df["explanation"].apply(polarity)
+# path = "experiments\\independent\\bilstm_mlp_improve-dnn15-1-25-decay0.0-L2-dr0.3-eval1-rake-polarity-improveloss_mean-alpha0.7-c-e40-2020-05-26_01-06-16\\explanations"
 # print(os.path.exists(path))
-df.to_csv("experiments\\independent\\bilstm_mlp_improve-dnn15-1-25-decay0.0-L2-dr0.3-eval1-rake-polarity-improveloss_mean-alpha0.7-c-e40-2020-05-26_01-06-16\\explanations\\result.csv")
+# df.to_csv("experiments\\independent\\bilstm_mlp_improve-dnn15-1-25-decay0.0-L2-dr0.3-eval1-rake-polarity-improveloss_mean-alpha0.7-c-e40-2020-05-26_01-06-16\\explanations\\result.csv")
+
