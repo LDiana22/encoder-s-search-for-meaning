a3116aec41d454da383b878d94acfc168a603c7c
LDiana22 <diana.lucaci22@yahoo.com>
2020-08-04 19:57:45 -0400

    tokenize and encode reviews

--- corpus_explanation/notebooks/qualitative_grep.py
+++ corpus_explanation/notebooks/qualitative_grep.py
@@ -86,6 +86,102 @@ CONFIG={
         "restore_checkpoint" : True,
         "checkpoint_file": "experiments/soa-dicts/bilstm_mlp_improve_15-25_l20.1_dr0.5_soa_vlstm2-256-0.5_pretrained_rake-4-600-dnn15-1-25-decay0.0-L2-dr0.5-eval1-rake-inst-4-600-improveloss_mean-alpha0.7-c-e30-2020-06-17_14-52-49/snapshot/2020-06-17_16-02-07_e6"
     }
+
+class IMDBDataset:
+
+  def __init__(self, args, max_length=250):
+    self.args = args
+    self.max_sent_len = 800
+    TEXT = data.Field(lower=True, 
+                      include_lengths=True,
+                      sequential=True,
+                      tokenize='spacy',
+                      # use_vocab=False,
+                      preprocessing=preprocess_pipeline)
+    LABEL = data.LabelField(dtype = torch.float)#, use_vocab=False)
+    print("Loading the IMDB dataset...")
+    # self.train_data = self._load_data(TEXT, LABEL, "../.data/imdb/aclImdb", "train")
+    # self.test_data = self._load_data(TEXT, LABEL, "../.data/imdb/aclImdb", "test")
+    self.train_data = self._load_data(TEXT, LABEL, IMDB_PATH, "train")
+    # self.test_data = self._load_data(TEXT, LABEL, IMDB_PATH, "test")
+    self.train_data, self.valid_data = self.train_data.split(random_state=random.getstate())
+
+    # start = datetime.now()
+    # formated_date = start.strftime(DATE_FORMAT)
+    # with open(f"train-imdb-{formated_date}", "w") as f:
+    #     f.write("\n".join([f"{' '.join(self.train_data[i].text)} ~  {self.train_data[i].label}" for i in range(len(self.train_data))]))
+    # with open(f"val-imdb-{formated_date}", "w") as f:
+    #     f.write("\n".join([f"{' '.join(self.valid_data[i].text)} ~  {self.valid_data[i].label}" for i in range(len(self.valid_data))]))
+    # with open(f"test-imdb-{formated_date}", "w") as f:
+    #     f.write("\n".join([f"{' '.join(self.test_data[i].text)} ~  {self.test_data[i].label}" for i in range(len(self.test_data))]))
+
+    print("IMDB...")
+    print(f"Train {len(self.train_data)}")
+    print(f"Valid {len(self.valid_data)}")
+    # print(f"Test {len(self.test_data)}")
+    TEXT.build_vocab(self.train_data,
+                 max_size = args["max_vocab_size"])
+    LABEL.build_vocab(self.train_data)
+
+    self.TEXT = TEXT
+    self.device = torch.device('cuda' if args["cuda"] else 'cpu')
+
+  def _load_data(self, text_field, label_field, path, data_type="train"):
+    fields = [('text', text_field), ('label', label_field)]
+    examples = []
+    path = os.path.join(path, data_type)
+    for label in ['pos', 'neg']:
+        print(f"{os.path.join(path, label, f'{label}.txt')}")
+        fname = os.path.join(path, label, f'{label}.txt')
+        with io.open(fname, 'r', encoding='utf-8', errors='replace') as f:
+            for text in f:
+                if text != '\n':
+                    sent_len = len(text.split())
+                    if sent_len > self.max_sent_len:
+                        self.max_sent_len = sent_len
+                    examples.append(data.Example.fromlist([text, label], fields))
+                if self.args["toy_data"] and (len(examples)==50 or len(examples)==100):
+                    break
+
+    print(f'Loaded {len(examples)}')
+    fields = dict(fields)
+    # Unpack field tuples
+    for n, f in list(fields.items()):
+        if isinstance(n, tuple):
+            fields.update(zip(n, f))
+            del fields[n]
+    return data.Dataset(examples, fields)
+
+  def iterators(self):
+    """
+      Returns train_iterator, valid_iterator, test_iterator
+    """
+    return data.BucketIterator.splits(
+      (self.train_data, self.valid_data, self.test_data), 
+      batch_size = self.args["batch_size"],
+      sort_within_batch = True,
+      sort_key=lambda x: len(x.text),
+      device = self.device)
+
+  def training(self):
+    return self.train_data
+
+  def get_training_corpus(self):
+    self.corpus = {"pos":[], "neg":[]}
+    self.corpus["pos"] = [" ".join(example.text) for example in self.train_data if example.label == "pos"]
+    self.corpus["neg"] = [" ".join(example.text) for example in self.train_data if example.label == "neg"]
+    return self.corpus
+
+  def dev(self):
+    return self.valid_data
+
+  def test(self):
+    return self.test_data 
+
+  def override(self, args):
+    self.args.update(args)
+    return self
+
 class AbstractModel(nn.Module):
     """
     Abstract Model
@@ -458,9 +554,16 @@ class FrozenVLSTM(AbstractModel):
 
         return metrics
 
+dataset = IMDBDataset(CONFIG)
+
 model = FrozenVLSTM(f"raw-pred-vanilla-lstm", MODEL_MAPPING, CONFIG)
 
-vanilla = model.load_checkpoint(checkpoint)
+model.load_checkpoint(checkpoint)
+import spacy
+nlp = spacy.load('en')
+
+def prepare_text_for_classification(texts):
+    return [torch.LongTensor([dataset.TEXT.vocab.stoi[t] for t in [tok.text for tok in nlp.tokenizer(sentence)]]).to(CONFIG["device"]).unsqueeze(1) for text in texts]
 
 ##################################################################
 def fix_file(path):
@@ -496,7 +599,7 @@ def load_explanations(path):
     df["raw_pred"] = df["explanation"].apply(lambda x: re.findall(r'\d+\.\d+', str(x))).apply(lambda x: float(x[3]) if len(x)>3 else None)
     import ipdb
     ipdb.set_trace(context=10)
-    df["vanilla_prediction"] = vanilla(df["review"])
+    df["vanilla_prediction"] = vanilla(torch.tensor(prepare_text_for_classification(df["review"])))
     return df
 ##################################################################
 
