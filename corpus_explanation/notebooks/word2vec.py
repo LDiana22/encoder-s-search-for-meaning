747bb1f7c3f47ca342a134fc7db126f1347d858a
LDiana22 <diana.lucaci22@yahoo.com>
2020-08-13 15:05:29 -0400

    fix indentation

--- corpus_explanation/notebooks/word2vec.py
+++ corpus_explanation/notebooks/word2vec.py
@@ -7,7 +7,7 @@ from gensim.test.utils import datapath
 import gensim.downloader as api
 DATA_PATH = "../.data/imdb/aclImdb/train/pos/pos.txt"
 with open(DATA_PATH) as file:
-    text_review = file.read()
+	text_review = file.read()
 
 model = api.load("word2vec-google-news-300")  # download the model and return as object ready for use
 word_vectors = model.wv
@@ -17,29 +17,29 @@ word_vectors = model.wv
 #model = word2vec.Word2Vec.load("w2v")
 
 def text_to_wordlist(text, remove_stopwords=True):
-    # 2. Remove non-letters
-    review_text = re.sub("[^a-zA-Z]", " ", text)
+	# 2. Remove non-letters
+	review_text = re.sub("[^a-zA-Z]", " ", text)
 
-    # 3. Convert words to lower case and split them, clean stopwords from model' vocabulary
-    words = review_text.lower().split()
-    stops = set(stopwords.words('english'))
-    meaningful_words = [w for w in words if not w in stops]
-    return (meaningful_words)
+	# 3. Convert words to lower case and split them, clean stopwords from model' vocabulary
+	words = review_text.lower().split()
+	stops = set(stopwords.words('english'))
+	meaningful_words = [w for w in words if not w in stops]
+	return (meaningful_words)
 
 
 # Function to get feature vec of words
 def get_feature_vec(words, model):
-    # Index2word is a list that contains the names of the words in
-    # the model's vocabulary. Convert it to a set, for speed 
-    #index2word_set = set(model.index2wordword2vec.Word2Vec.load_word2vec_format)
-    index2word_set = model.wv
-    clean_text = []
-    # vocabulary, add its feature vector to the total
-    for word in words:
-        if word in index2word_set:
-            clean_text.append(model[word])
+	# Index2word is a list that contains the names of the words in
+	# the model's vocabulary. Convert it to a set, for speed 
+	#index2word_set = set(model.index2wordword2vec.Word2Vec.load_word2vec_format)
+	index2word_set = model.wv
+	clean_text = []
+	# vocabulary, add its feature vector to the total
+	for word in words:
+		if word in index2word_set:
+			clean_text.append(model[word])
 
-    return clean_text
+	return clean_text
 
 print("Cleaning text...")
 # bag of word list without stopwords
@@ -48,10 +48,10 @@ clean_train_text = (text_to_wordlist(text_review, remove_stopwords=True))
 # delete words which occur more than ones
 clean_train = []
 for words in clean_train_text:
-    if words in clean_train:
-        words = +1
-    else:
-        clean_train.append(words)
+	if words in clean_train:
+		words = +1
+	else:
+		clean_train.append(words)
 print("Getting feature vectors")
 trainDataVecs = get_feature_vec(clean_train, model)
 trainData = numpy.asarray(trainDataVecs)
@@ -83,11 +83,11 @@ cosine = cosine.T * inv_mag
 
 # pagerank powermethod
 def powerMethod(A, x0, m, iter):
-    n = A.shape[1]
-    delta = m * (array([1] * n, dtype='float64') / n)
-    for i in range(iter):
-        x0 = dot((1 - m), dot(A, x0)) + delta
-    return x0
+	n = A.shape[1]
+	delta = m * (array([1] * n, dtype='float64') / n)
+	for i in range(iter):
+		x0 = dot((1 - m), dot(A, x0)) + delta
+	return x0
 
 
 n = cosine.shape[1]  # A is n x n
@@ -102,8 +102,8 @@ print("Printing wordlist")
 keywords_list = []
 
 for words in a:
-    keywords_list.append(clean_train_text[words])
-    
+	keywords_list.append(clean_train_text[words])
+		
 print(keywords_list)
 
 vocab = []
@@ -115,8 +115,8 @@ while i <len(a):
 		phrase = f"{phrase} {c_word}"
 		i+=1
 		c_word = a[i]
-  vocab +=phrase
-  i=i+1
+	vocab +=phrase
+	i=i+1
 
 print(vocab)
 
